fb530089c6405f3d95a0c2294a7c342078c8b160
Ashwin R C <ashwin2001achu@gmail.com>
2020-04-09 03:15:34 +0000

    guacamole: include overlay-potato for overlays

--- device.mk
+++ device.mk
@@ -14,7 +14,8 @@ $(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-potato
 
 # Audio
 PRODUCT_COPY_FILES += \
